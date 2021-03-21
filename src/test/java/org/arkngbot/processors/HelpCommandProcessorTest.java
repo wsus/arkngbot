69388917a261dd956e4ce924e714bd3b58bf0322
wsus <wsus90@gmail.com>
2021-03-21 20:52:35 +0100

    changed target release to 12; replaced text blocks with traditional strings

--- src/test/java/org/arkngbot/processors/HelpCommandProcessorTest.java
+++ src/test/java/org/arkngbot/processors/HelpCommandProcessorTest.java
@@ -11,13 +11,12 @@ import static org.hamcrest.Matchers.is;
 
 public class HelpCommandProcessorTest {
 
-    private static final String HELP_TEXT = """
-            Available commands:
-            `/arkng wiki <query>`: Searches the UESP wiki and returns top five results. If you provide the namespace resulting in a direct hit (for example `/arkng wiki online:divayth fyr`), a direct link to the result will be returned.
-            `/arkng wisdom`: Returns a random paragraph from a random TES lorebook.
-            `/arkng ttc search <query>`: First tries to determine the item from the query using TTC's autocompletion feature, then returns a link to the search results of that item.
-            `/arkng ttc price <query>`: First tries to determine the item from the query using TTC's autocompletion feature, then returns a price check for that item.
-            `/arkng help`: Returns the list of all commands.""";
+    private static final String HELP_TEXT = "Available commands:"
+            + "/arkng wiki <query>`: Searches the UESP wiki and returns top five results. If you provide the namespace resulting in a direct hit (for example `/arkng wiki online:divayth fyr`), a direct link to the result will be returned."
+            + "/arkng wisdom`: Returns a random paragraph from a random TES lorebook."
+            + "/arkng ttc search <query>`: First tries to determine the item from the query using TTC's autocompletion feature, then returns a link to the search results of that item."
+            + "/arkng ttc price <query>`: First tries to determine the item from the query using TTC's autocompletion feature, then returns a price check for that item."
+            + "/arkng help`: Returns the list of all commands.";
     private static final String HELP = "help";
     private static final String OTHER = "other";
 
